9ea75f3b30021dc5aac3b4b1944052205373dc1f
user <Savenko19@gmail.com>
2018-05-24 15:16:58 +0700

    add node class

---
+++ app/src/main/java/com/example/user/votechain/Blockchain/Node.java
@@ -0,0 +1,15 @@
+package com.example.user.votechain.Blockchain;
+
+import android.net.Uri;
+
+public class Node {
+    private Uri address;
+
+    public Uri getAddress() {
+        return address;
+    }
+
+    public void setAddress(Uri address) {
+        this.address = address;
+    }
+}
